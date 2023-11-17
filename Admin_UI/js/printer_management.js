01b225097e81ffdae5a872b901744c8c22d7dc81
hieugiaosu <phamthehieuvtt123@gmail.com>
2023-11-17 22:57:25 +0700

    from TheHieu, add some basic admin web UI

---
+++ Admin_UI/js/printer_management.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#menu').html(getMenuContent())
+    $('#account_bar').html(getAccountBarContent())
+    $('#printer_management_button').css("background-color","#C8C2F2")
+});
