f3e038d6aba1fc7b84c911fe1025d5ba46ac31da
hieugiaosu <phamthehieuvtt123@gmail.com>
2023-11-29 17:00:56 +0700

    edit database, add communication from be to fe in admin

--- Admin_UI/js/printer_detail.js
+++ Admin_UI/js/printer_detail.js
@@ -2,8 +2,90 @@ function showSuccessToast(){
     let toast = new bootstrap.Toast($('#successToast'))
     toast.show()
 }
+let resetinput = async function(res){
+    $(".changable>input").css('color','black')
+    $("#location>input").val(res.ViTri)
+    $("#status>input").val(res.TinhTrang)
+    $("#type_printer>input").val(res.Kieu)
+    $("#speed>input").val(res.TocDoIn )
+    $("#memory>input").val(res.BoNho )
+    
+    // $("#resolution>input").val(res.D)
+    $("#support_color>input").val((res.InMau)?"Có":"Không")
+    if (res.InMau){
+        $("#yes").prop("checked",true)
+    } else {
+        $("#no").prop("checked",true)
+    }
+    $("#capacity>input").val(res.KhayGiay)
+    $("#ink>input").val(res.LoaiMuc)
+    $("#watt>input").val(res.CongSuat)
+    $("#weight>input").val(res.TrongLuong)
+    $("#shape>input").val(res.KichThuoc)
 
-$(document).ready(function(){
+    }
+
+function updateInfo(res) {
+    $("#firm-name").html(res.Hang)
+    $("#printer_id").html(res.ID)
+    $("#printer_model").html(res.Model)
+    $("#location>span").html(res.ViTri)
+    $("#status>span").html((res.TinhTrang=="Working")?"Kích hoạt":"Vô hiệu hóa")
+    $("#type_printer>span").html(res.Kieu)
+    $("#speed>span").html(res.TocDoIn )
+    $("#memory>span").html(res.BoNho )
+    $("#shape>span").html(res.KichThuoc)
+    $("#capacity>span").html(res.KhayGiay)
+    $("#ink>span").html(res.LoaiMuc)
+    $("#watt>span").html(res.CongSuat)
+    $("#support_color>span").html((res.InMau)?"Có":"Không")
+    $("#weight>span").html(res.TrongLuong )
+}
+
+$(document).ready(async function(){
+    let printerID = (new URLSearchParams(window.location.search)).get('printerid')
+    let res = await (await fetch(`http://localhost:3001/printers/${printerID}`)).json()
+    updateInfo(res)
+    // $("#resolution>span").html(res.D)
+    
+    await resetinput(res)
+    $("#reset").click(async ()=>{
+        await resetinput(res)}
+    )
+
+    $("#submit").click(async function(){
+        let data = {
+            'KhayGiay' : $("#capacity>input").val(),
+            'LoaiMuc' : $("#ink>input").val(),
+            'ViTri' : $("#location>input").val(),
+            'TinhTrang' : $("#status>span").text() == 'Kích hoạt' ? 'Working' : 'Disabled',
+            'InMau' : $("#yes").is(":checked"),
+            'CongSuat' : $("#watt>input").val(),
+            'TrongLuong' : $("#weight>input").val(),
+            'Kieu' : $("#type_printer>input").val(),
+            'TocDoIn' : $("#speed>input").val(),
+            'KichThuoc' : $("#shape>input").val(),
+            'BoNho' : $("#memory>input").val(),
+            'AnhMayIn' : 'https://s3.pricemestatic.com/Large/Images/RetailerProductImages/StRetailer1450/rp_39470408_0021477728_l.png',
+            'DoPhanGiai' : $("#resolution>span").text(),
+        }
+        let id = $('#printer_id').text()
+        let res = await fetch(`http://localhost:3001/printers/${id}`, {
+            method:'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        let temp = await res.json()
+        res = await (await fetch(`http://localhost:3001/printers/${id}`)).json()
+        $('#editmode').click()
+        updateInfo(res)
+        if (temp.message=="Printer saved successfully") {
+            showSuccessToast()
+        }
+    })
+
     $('.back').click(function(){
         window.location.href = 'printer_management.html'
     })
@@ -17,12 +99,10 @@ $(document).ready(function(){
             $('.btn-check').not(this).prop('checked',!this.checked)
         })
     })
+
     $('.form-control').change(function(){
         $(this).css('color','red')
     })
-    $(document).keydown(function(e){
-        if (e.key=='s') {
-            showSuccessToast()
-        }
-    })
+    
+    
 })
--- Admin_UI/js/printer_management.js
+++ Admin_UI/js/printer_management.js
@@ -1,17 +1,30 @@
 /*
 This function is to get printer data from the server
 */
-function getPrinterInfo(){
+async function getPrinterInfo(){
     // implement
-    return {0:{'ID':'0001','model':'aaaa','location':"H1",'status':1},1:{'ID':'0002','model':'aaaa','location':"H6",'status':0}};
+    let tmp = await fetch('http://localhost:3001/printers');
+    let res = await tmp.json();
+    // .then(
+    //     (res) => res.json()
+    // )
+    // .then((data) => {
+    //     res = data;
+    //     console.log(res)
+    // })
+    // return [{'ID':'0001','model':'aaaa','location':"H1",'status':1},{'ID':'0002','model':'aaaa','location':"H6",'status':0}];
+    console.log(res)
+    return res
 }
 
 /*
 this play general 
 */
-function displayPrinterInfo(begin,end){
-    let printerInfo =getPrinterInfo()
-    for (printer in printerInfo){
+async function displayPrinterInfo(begin,end){
+    let printerInfo = await getPrinterInfo()
+    // console.log(printerInfo)
+    // $("tbody").html('')
+    for (let printer=0;printer<printerInfo.length;printer++){
         let row = `
             <tr id="row${printer}">
             <td scope="col pt-0" class="checkSingle">
@@ -21,38 +34,52 @@ function displayPrinterInfo(begin,end){
             ${Number(printer)+1}
             </td>
             <td scope="col pt-0" class="printer_list_data">
-            ${printerInfo[printer].model}
+            ${printerInfo[printer].Model}
             </td>
             <td scope="col pt-0" class="printer_list_data" id="${printerInfo[printer].ID}">
             ${printerInfo[printer].ID}
             </td>
             <td scope="col pt-0" class="printer_list_data">
-            ${printerInfo[printer].location}
+            ${printerInfo[printer].ViTri}
             </td>
             <td scope="col pt-0" class="printer_list_data text-center">
-                <span class="${(printerInfo[printer].status)?'enable':'disable'} px-2">
-                ${(printerInfo[printer].status)?'Hoạt động':'Vô hiệu hóa'}
+                <span class="${(printerInfo[printer].TinhTrang == 'Working')?'enable':'disable'} px-2">
+                ${(printerInfo[printer].TinhTrang == 'Working')?'Hoạt động':'Vô hiệu hóa'}
                 </span>
             </td>
             <td scope="col pt-0 my-0 p-0" class="text-center">
+            <a href="printer_detail.html?printerid=${printerInfo[printer].ID}">
                 <img src="image/admin_printer/info.png" alt="info" class="infoPrinterBtn" id="info${printer}">
+            </a>    
                 <img src="image/admin_printer/delete.png" alt="del" class="delPrinterBtn" id="del${printer}">
             </td>
             <td scope="col pt-0">
-            <div class="form-check form-switch p-0 m-0">
-                <input class="form-check-input toggle_status w-50" type="checkbox" role="switch" id="toggle${printer}"${(printerInfo[printer].status)?" checked":""}>
+            <div class="form-check form-switch p-0 m-0 toggle-status">
+                <input class="form-check-input toggle_status w-50" type="checkbox" value=1 role="switch" id="toggle${printerInfo[printer].ID}"${(printerInfo[printer].TinhTrang=="Working")?" checked":""} onchange="toggleStatus('${printerInfo[printer].ID}')">
             </div>
             </td>
             </tr>
         `
         $('tbody').append(row)
-        if (printerInfo[printer].status == 0){
+        if (printerInfo[printer].TinhTrang == 'Disabled'){
             let rowid = "#row"+printer
             $(rowid).css('color','#B2BEC3')
         }
     }
 }
-
+function toggleStatus(id){
+    fetch(`http://localhost:3001/printers/${id}/status`,{method:"PUT"})
+    .then(res=>res.json())
+    .then(data => {
+        showSuccessToast()
+        $('tbody').html('')
+        displayPrinterInfo(0,10)
+    })
+    .catch(err => {
+        showFailToast()
+    })
+    
+}
 function showSuccessToast(){
     let toast = new bootstrap.Toast($('#successToast'))
     toast.show()
@@ -71,6 +98,9 @@ $(document).ready(function() {
     })
     
     displayPrinterInfo(0,10)
+    $('.delPrinterBtn').click(function(){
+        console.log(1)
+    })
     $('#checkAll').click(function(){
         $('.checkSingle>input').not(this).prop('checked', this.checked)
     })
@@ -84,12 +114,6 @@ $(document).ready(function() {
         }
     });
 
-    $('.infoPrinterBtn').click(function (){
-        let rowID = 'row'+this.id.slice(4)
-        let printerId = $('#'+rowID+'>td')[3].id
-        window.location.href='printer_detail.html?printerid='+printerId
-    })
-
     $(document).keydown(function(e){
         if (e.key=='s') {
             showSuccessToast()
@@ -99,5 +123,40 @@ $(document).ready(function() {
     })
     $('.btn-check').click(function(){
         $('.btn-check').not(this).prop('checked',!this.checked)
+    })
+    $('#toggleMI0001').click(function(){
+        console.log(1)
+    })
+
+    $('#upload-image>button').click(function() {
+        
+    })
+
+    $('form').submit(function (e) {
+        e.preventDefault()
+
+        let data = {
+            'Hang' : e.target['firm'].value,
+            'KhayGiay' : $("#capacity>input").val(),
+            'LoaiMuc' : $("#ink>input").val(),
+            'ViTri' : $("#location>input").val(),
+            'TinhTrang' : $("#status>span").text() == 'Kích hoạt' ? 'Working' : 'Disabled',
+            'InMau' : $("#yes").is(":checked"),
+            'CongSuat' : $("#watt>input").val(),
+            'TrongLuong' : $("#weight>input").val(),
+            'Kieu' : $("#type_printer>input").val(),
+            'TocDoIn' : $("#speed>input").val(),
+            'KichThuoc' : $("#shape>input").val(),
+            'BoNho' : $("#memory>input").val(),
+            'AnhMayIn' : 'https://s3.pricemestatic.com/Large/Images/RetailerProductImages/StRetailer1450/rp_39470408_0021477728_l.png',
+            'DoPhanGiai' : $("#resolution>span").text(),
+        }
+
+        for (let index = 0; index < e.target.length; index++) {
+            // const element = array[index];
+            console.log(index, e.target[index])
+            
+        }
+        console.log(e.target['firm'].value)
     })
 });
