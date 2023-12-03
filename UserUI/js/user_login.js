7b8cb7274d2b2a1be83bc6c9548eac481be10acf
Nguyen Xuan Tho <tho.nguyenxuantho573@hcmut.edu.vn>
2023-12-03 09:04:25 +0700

    Reconfig files

--- UserUI/js/user_login.js
+++ UserUI/js/user_login.js
@@ -2,14 +2,14 @@ const $password = document.querySelector("#passid");
 const $toggler = document.querySelector("i");
 
 const showHidePassword = () => {
-  if ($password.getAttribute("type") === "password") {
-    $password.setAttribute("type", "text");
-  } else {
-    $password.setAttribute("type", "password");
-  }
+	if ($password.getAttribute("type") === "password") {
+		$password.setAttribute("type", "text");
+	} else {
+		$password.setAttribute("type", "password");
+	}
 
-  $toggler.classList.toggle("fa-eye");
-  $toggler.classList.toggle("fa-eye-slash");
+	$toggler.classList.toggle("fa-eye");
+	$toggler.classList.toggle("fa-eye-slash");
 };
 
 $toggler.addEventListener("click", showHidePassword);
